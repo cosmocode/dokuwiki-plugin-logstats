6dfa98e79894fbd366e5fb36ba63a0a535932ec9
Jean-Françcois Lalande <jean-francois.lalande@ensi-bourges.fr>
2012-11-21 14:32:25 +0100

    initial commit 1.4 (14/06/2008)

---
+++ conf/metadata.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+    * Metadata for configuration manager plugin
+    * Additions for the logstats plugin
+    *
+    * @author    J.-F. Lalande <jf@lalande.nom.fr>
+    */
+
+    $meta['accesslog']    = array('string');
+    $meta['banned_ip']  = array('string');
+    $meta['banned_users']  = array('string');
+    $meta['banned_agents'] = array('string');
+?>
