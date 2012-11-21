8f499cad4623c888e7acad72440f4a39aa84b8c6
Andreas Gohr <gohr@cosmocode.de>
2012-11-21 14:47:49 +0100

    general clean up

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,13 +1,12 @@
 <?php
-    /**
-    * Metadata for configuration manager plugin
-    * Additions for the logstats plugin
-    *
-    * @author    J.-F. Lalande <jf@lalande.nom.fr>
-    */
+/**
+ * Metadata for configuration manager plugin
+ * Additions for the logstats plugin
+ *
+ * @author    J.-F. Lalande <jf@lalande.nom.fr>
+ */
 
-    $meta['accesslog']    = array('string');
-    $meta['banned_ip']  = array('string');
-    $meta['banned_users']  = array('string');
-    $meta['banned_agents'] = array('string');
-?>
+$meta['accesslog']     = array('string');
+$meta['banned_ip']     = array('string');
+$meta['banned_users']  = array('string');
+$meta['banned_agents'] = array('string');
