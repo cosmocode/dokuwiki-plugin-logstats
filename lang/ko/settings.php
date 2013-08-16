ed54f06fb86ea31962b9e3fd9a6190a6f1e70eb4
Myeongjin <aranet100@gmail.com>
2013-08-16 10:50:04 +0200

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['accesslog']             = '접근 기록은 어디에 저장해야 합니까? 상대 경로는 <code>data/meta/</code>를 기준으로 합니다.';
+$lang['banned_ip']             = '로그인하지 않아야 하는 IP의 쉼표로 구분된 목록.';
+$lang['banned_users']          = '로그인하지 않아야 하는 사용자 이름의 쉼표로 구분된 목록.';
+$lang['banned_agents']         = '로그인하지 않아야 하는 사용자 에이전트의 쉼표로 구분된 목록.';
