e6f5750ae2a15e262e1b146e0f2c24538720c3bf
DelDadam <deldadam@gmail.com>
2014-09-19 20:40:04 +0200

    translation update

---
+++ lang/hu/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author DelDadam <deldadam@gmail.com>
+ */
+$lang['accesslog']             = 'Hová mentsük a naplófájlt? Relatív útvonal a <code>data/meta/</code> útvonalhoz képest értendő.';
+$lang['banned_ip']             = 'Nem naplózandó IP-címek vesszővel elválasztott listája.';
+$lang['banned_users']          = 'Nem naplózandó felhasználói nevek vesszővel elválasztott listája.';
+$lang['banned_agents']         = 'Nem naplózandó felhasználói ágensek (böngészők, robotok) vesszővel elválasztott listája.';
