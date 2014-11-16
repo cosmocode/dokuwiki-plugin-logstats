b2965c833f49276994d3e5abf8ae5945752b3118
HL <tohelong@gmail.com>
2014-11-16 13:25:04 +0100

    translation update

---
+++ lang/zh/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author HL <tohelong@gmail.com>
+ */
+$lang['accesslog']             = '日志文件的保存位置,相对于<code>data/meta/</code>';
+$lang['banned_ip']             = '不记录日志的ip, 用英文逗号隔开';
+$lang['banned_users']          = '不记录日志的用户名, 用英文逗号隔开';
+$lang['banned_agents']         = '不记录日志的用户代理?没搞懂,用英文逗号隔开';
