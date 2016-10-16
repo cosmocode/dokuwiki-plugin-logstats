54fba704fe70615cc2298d05beb780524ff44997
Arne Hanssen <arne.hanssen@getmail.no>
2016-10-16 11:15:29 +0200

    translation update

---
+++ lang/no/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Arne Hanssen <arne.hanssen@getmail.no>
+ */
+$lang['accesslog']             = 'Hvor skal tilgangsloggen lagres? Relative stier er relativt til <code>data/meta/</code>.';
+$lang['banned_ip']             = 'Kommaseparert liste over IP-adresser som ikke skal logges.';
+$lang['banned_users']          = 'Kommaseparert liste over brukernavn som ikke skal logges.';
+$lang['banned_agents']         = 'Kommaseparert liste over brukeragenter som ikke skal logges.';
