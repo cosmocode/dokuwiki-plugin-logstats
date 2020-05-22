917ba79473ad788fb6649fd5b34f82d8af242984
Jacob Palm <jacobpalmdk@icloud.com>
2020-05-23 00:05:05 +0200

    translation update

---
+++ lang/da/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Jacob Palm <jacobpalmdk@icloud.com>
+ */
+$lang['accesslog']             = 'Hvor skal adgangslog gemmes? Relative stier er relative til <code>data/meta/</code>.';
+$lang['banned_ip']             = 'Kommasepareret liste over IP-adresser der ikke skal logges.';
+$lang['banned_users']          = 'Kommasepareret liste over bruger der ikke skal logges.';
+$lang['banned_agents']         = 'Kommasepareret liste over user agents der ikke skal logges.';
