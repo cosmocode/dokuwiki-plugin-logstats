3707fddcd9a2731db1443ace82afd7be610436bc
Aleksandr Selivanov <alexgearbox@yandex.ru>
2020-09-20 19:15:33 +0200

    translation update

---
+++ lang/ru/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['accesslog']             = 'Где сохранять журнал доступа? Относительный путь относительно <code>data/meta/</code>';
+$lang['banned_ip']             = 'Список IP-адресов, которые не&nbsp;должны регистрироваться (перечислите через запятую).';
+$lang['banned_users']          = 'Список имён пользователей, которые не&nbsp;должны регистрироваться (перечислите через запятую).';
+$lang['banned_agents']         = 'Список User-agent браузеров, которые не&nbsp;должны регистрироваться (перечислите через запятую).';
