b094c17f35d5de3fe04b8ec1151f44b8bfb23013
Minh <phandinhminh@protonmail.ch>
2023-02-19 16:40:16 +0100

    translation update

---
+++ lang/vi/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Minh <phandinhminh@protonmail.ch>
+ */
+$lang['accesslog']             = 'Nhật ký log truy cập nên được lưu tại đâu? Các đường dẫn tương đối liên quan tới <code>data/meta/</code>.';
+$lang['banned_ip']             = 'Dấu phẩy ngăn cách danh sách các IP mà không nên được lưu log lại.';
+$lang['banned_users']          = 'Dấu phẩy ngăn cách danh sách các tên người dùng mà không nên được lưu log lại.';
+$lang['banned_agents']         = 'Dấu phẩy ngăn cách danh sách các tác nhân agent người dùng mà không nên được lưu log lại.';
