746fbadb43ee870223dc09ea153a7b86da2052c6
Wizzard <wizzardsk@gmail.com>
2026-06-04 20:36:13 +0200

    Translation update (sk)

---
+++ lang/sk/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['accesslog']             = 'Kam sa má ukladať access log? Relatívne cesty sú relatívne k <code>data/meta/</code>.';
+$lang['banned_ip']             = 'Čiarkami oddelený zoznam IP adries, ktoré sa nemajú logovať.';
+$lang['banned_users']          = 'Čiarkami oddelený zoznam používateľských mien, ktoré sa nemajú logovať.';
+$lang['banned_agents']         = 'Čiarkami oddelený zoznam user agentov, ktoré sa nemajú logovať.';
